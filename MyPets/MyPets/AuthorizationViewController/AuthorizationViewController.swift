f2b408622b7ba3c2526f0bcdc9f3f8355fcbfc70
AhmerovDmitry <62261655+AhmerovDmitry@users.noreply.github.com>
2020-10-09 19:47:42 +0300

    First update for build app

---
+++ MyPets/MyPets/AuthorizationViewController/AuthorizationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MyPets
+//
+//  Created by Дмитрий Ахмеров on 09.10.2020.
+//
+
+import UIKit
+
+class AuthorizationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
