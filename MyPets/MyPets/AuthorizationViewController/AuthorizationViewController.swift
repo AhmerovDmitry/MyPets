c8abc47851191088c9b0ce9f0de56c6bc939c6d0
AhmerovDmitry <62261655+AhmerovDmitry@users.noreply.github.com>
2020-10-10 00:01:46 +0300

    Update
    First screen UI completed

--- MyPets/MyPets/AuthorizationViewController/AuthorizationViewController.swift
+++ MyPets/MyPets/AuthorizationViewController/AuthorizationViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AuthorizationViewController.swift
 //  MyPets
 //
 //  Created by Дмитрий Ахмеров on 09.10.2020.
@@ -8,10 +8,145 @@
 import UIKit
 
 class AuthorizationViewController: UIViewController {
+    private let logoStackView = UIStackView()
+    private let logoLabel = UIImageView()
+    private let textLabel = UIImageView()
+    
+    private let authorizationStackView = UIStackView()
+    private let logInButton = UIButton(type: .system)
+    private let signInButton = UIButton(type: .system)
+    
+    private let socialNetworksLabel = UILabel()
+    private let textLine = UILabel()
+    
+    private let socialIconsStackView = UIStackView()
+    private let facebookButton = UIButton(type: .custom)
+    private let okButton = UIButton(type: .custom)
+    private let vkButton = UIButton(type: .custom)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        setup()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
 
 }
 
+extension AuthorizationViewController: AuthorizationViewControllerProtocol {
+    func setup() {
+        setupConstraint()
+        setupViewsAndLabels()
+    }
+    
+    func setupConstraint() {
+        //MARK: - Logo Constraints
+        view.addSubview(logoStackView)
+        logoStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 149).isActive = true
+        logoStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        [logoLabel, textLabel].forEach({ logoStackView.addArrangedSubview($0) })
+        
+        logoLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        logoLabel.heightAnchor.constraint(equalToConstant: 124).isActive = true
+        
+        textLabel.widthAnchor.constraint(equalToConstant: 141).isActive = true
+        textLabel.heightAnchor.constraint(equalToConstant: 39).isActive = true
+        
+        //MARK: - Authorization Constraints
+        view.addSubview(authorizationStackView)
+        authorizationStackView.topAnchor.constraint(equalTo: logoStackView.bottomAnchor, constant: 22).isActive = true
+        authorizationStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        [signInButton, logInButton].forEach({ authorizationStackView.addArrangedSubview($0) })
+        
+        signInButton.widthAnchor.constraint(equalToConstant: 311).isActive = true
+        signInButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        logInButton.widthAnchor.constraint(equalToConstant: 311).isActive = true
+        logInButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        //MARK: - Social Networks Constraints
+        view.addSubview(textLine)
+        view.addSubview(socialNetworksLabel)
+        textLine.heightAnchor.constraint(equalToConstant: 0.5).isActive = true
+        textLine.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        textLine.topAnchor.constraint(equalTo: authorizationStackView.bottomAnchor, constant: 74).isActive = true
+        textLine.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
+        textLine.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16).isActive = true
+        
+        socialNetworksLabel.widthAnchor.constraint(equalToConstant: 213).isActive = true
+        socialNetworksLabel.heightAnchor.constraint(equalToConstant: 18).isActive = true
+        socialNetworksLabel.centerXAnchor.constraint(equalTo: textLine.centerXAnchor).isActive = true
+        socialNetworksLabel.centerYAnchor.constraint(equalTo: textLine.centerYAnchor).isActive = true
+        
+        //MARK: - Social Icons Constraints
+        view.addSubview(socialIconsStackView)
+        socialIconsStackView.topAnchor.constraint(equalTo: socialNetworksLabel.bottomAnchor, constant: 16).isActive = true
+        socialIconsStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+
+        [facebookButton, okButton, vkButton].forEach({ icon in
+            socialIconsStackView.addArrangedSubview(icon)
+            
+            icon.widthAnchor.constraint(equalToConstant: 44).isActive = true
+            icon.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        })
+    }
+    
+    func setupViewsAndLabels() {
+        [logoStackView, logoLabel, textLabel,
+         authorizationStackView, logInButton, signInButton,
+         socialNetworksLabel, textLine,
+         socialIconsStackView, facebookButton, okButton, vkButton].forEach({
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        })
+        
+        //MARK: - Logo Settings
+        logoStackView.alignment = .center
+        logoStackView.spacing = 15
+        logoStackView.axis = .vertical
+        
+        logoLabel.image = UIImage(named: "mainIcon")
+        textLabel.image = UIImage(named: "myPetsText")
+        
+        //MARK: - Authorization Settings
+        authorizationStackView.alignment = .center
+        authorizationStackView.spacing = 16
+        authorizationStackView.axis = .vertical
+        
+        signInButton.setTitle("Зарегистрироваться", for: .normal)
+        signInButton.backgroundColor = UIColor.CustomColor.purple
+        signInButton.titleLabel?.font = UIFont.systemFont(ofSize: 17)
+        signInButton.setTitleColor(.white, for: .normal)
+        signInButton.layer.cornerRadius = 25
+        
+        logInButton.setTitle("Войти", for: .normal)
+        logInButton.backgroundColor = .white
+        logInButton.titleLabel?.font = UIFont.systemFont(ofSize: 17)
+        logInButton.setTitleColor(UIColor.CustomColor.purple, for: .normal)
+        logInButton.layer.borderWidth = 1
+        logInButton.layer.borderColor = UIColor.CustomColor.purple.cgColor
+        logInButton.layer.cornerRadius = 25
+        
+        //MARK: - Social Networks Settings
+        socialIconsStackView.alignment = .center
+        socialIconsStackView.spacing = 16
+        socialIconsStackView.axis = .horizontal
+        
+        textLine.backgroundColor = .black
+        
+        socialNetworksLabel.text = "Войти через социальные сети"
+        socialNetworksLabel.font = UIFont.systemFont(ofSize: 14)
+        socialNetworksLabel.textAlignment = .center
+        socialNetworksLabel.backgroundColor = .white
+        
+        //MARK: - Social Networ Icon Settings
+        facebookButton.setImage(UIImage(named: "facebookIcon"), for: .normal)
+        okButton.setImage(UIImage(named: "okIcon"), for: .normal)
+        vkButton.setImage(UIImage(named: "vkIcon"), for: .normal)
+    }
+    
+}
---
+++ MyPets/UIColor.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor.swift
+//  MyPets
+//
+//  Created by Дмитрий Ахмеров on 09.10.2020.
+//
+
+import UIKit
+
+extension UIColor {
+    struct CustomColor {
+        static let purple = UIColor(red: 155/255, green: 81/255, blue: 224/255, alpha: 1.0)
+    }
+}
