bad18609cecf22d3c6560f459de323b16c724d1f
AhmerovDmitry <62261655+AhmerovDmitry@users.noreply.github.com>
2020-10-12 20:41:41 +0300

    Update
    Add skip button for OnboardingViewController

--- MyPets/MyPets/OnboardView/Onboarding/OnboardViewController.swift
+++ MyPets/MyPets/OnboardView/Onboarding/OnboardViewController.swift
@@ -12,6 +12,7 @@ class OnboardViewController: UIViewController, OnboardViewControllerProtocol {
     private let mainStackView = UIStackView()
     lazy var pageControl = UIPageControl()
     let doneButton = UIButton(type: .system)
+    let closeButton = UIButton(type: .system)
     let models = [
         OnboardModel(image: "onboardImage_1", text: "Вся информация о питомце всегда под рукой"),
         OnboardModel(image: "onboardImage_2", text: "Вы не забудите купить корм или сделать прививку"),
@@ -35,7 +36,7 @@ class OnboardViewController: UIViewController, OnboardViewControllerProtocol {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-                
+        
         collectionView.delegate = self
         collectionView.dataSource = self
         
@@ -52,38 +53,45 @@ class OnboardViewController: UIViewController, OnboardViewControllerProtocol {
         setupConstraint()
         setupViewsAndLabels()
     }
-
+    
     func setupConstraint() {
         view.addSubview(mainStackView)
+        view.addSubview(closeButton)
         [pageControl, doneButton].forEach({ mainStackView.addArrangedSubview($0) })
-
+        
         mainStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 142).isActive = true
         mainStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-
+        
         pageControl.widthAnchor.constraint(equalToConstant: 50).isActive = true
         pageControl.heightAnchor.constraint(equalToConstant: 8).isActive = true
-
+        
         doneButton.widthAnchor.constraint(equalToConstant: 311).isActive = true
         doneButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        closeButton.topAnchor.constraint(lessThanOrEqualTo: view.topAnchor, constant: 60).isActive = true
+        closeButton.rightAnchor.constraint(lessThanOrEqualTo: view.rightAnchor, constant: -32).isActive = true
+        closeButton.widthAnchor.constraint(lessThanOrEqualToConstant: 97).isActive = true
+        closeButton.heightAnchor.constraint(lessThanOrEqualToConstant: 21).isActive = true
     }
-
+    
     func setupViewsAndLabels() {
         [mainStackView,
          pageControl,
-         doneButton].forEach({
+         doneButton,
+         closeButton].forEach({
             $0.translatesAutoresizingMaskIntoConstraints = false
-        })
-
+         })
+        
         mainStackView.axis = .vertical
         mainStackView.alignment = .center
         mainStackView.spacing = 16
         mainStackView.setCustomSpacing(32, after: pageControl)
-
+        
         pageControl.currentPage = 0
         pageControl.numberOfPages = models.count
         pageControl.currentPageIndicatorTintColor = UIColor.CustomColor.purple
         pageControl.pageIndicatorTintColor = UIColor.CustomColor.lightGray
-
+        
         doneButton.setTitle("Далее", for: .normal)
         doneButton.backgroundColor = .white
         doneButton.titleLabel?.font = UIFont.systemFont(ofSize: 17)
@@ -92,6 +100,11 @@ class OnboardViewController: UIViewController, OnboardViewControllerProtocol {
         doneButton.layer.borderColor = UIColor.CustomColor.purple.cgColor
         doneButton.layer.cornerRadius = 25
         doneButton.addTarget(self, action: #selector(handleNext), for: .touchUpInside)
+        
+        closeButton.setTitle("Пропустить", for: .normal)
+        closeButton.titleLabel?.font = UIFont.systemFont(ofSize: 17)
+        closeButton.setTitleColor(UIColor.CustomColor.purple, for: .normal)
+        closeButton.addTarget(self, action: #selector(presentController), for: .touchUpInside)
     }
     
     deinit {
