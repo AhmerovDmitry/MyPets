471575475ad987a12fcdb36cd5db95d82e071709
AhmerovDmitry <ahmerov.dmitry@gmail.com>
2021-08-16 16:37:36 +0300

    Добавил новую модель питомца для CoreData

--- MyPets/App + Scene/AppDelegate.swift
+++ MyPets/App + Scene/AppDelegate.swift
@@ -35,7 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
          */
-        let container = NSPersistentContainer(name: "PetModel")
+        let container = NSPersistentContainer(name: "PetInformation")
         container.loadPersistentStores { _, error in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
---
+++ MyPets/MyPets/CoreData/OMPetInformation+CoreDataClass.swift
@@ -0,0 +1,14 @@
+//
+//  OMPetInformation+CoreDataClass.swift
+//  
+//
+//  Created by Дмитрий Ахмеров on 16.08.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(OMPetInformation)
+public class OMPetInformation: NSManagedObject {
+}
