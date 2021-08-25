4da09322081be7e3cf50886c9263fd8475a1f8e2
AhmerovDmitry <ahmerov.dmitry@gmail.com>
2021-08-25 17:41:49 +0300

    Системная кнопка для удаления моделей из CoreData

--- MyPets/MyPets/ProfileViewController/ProfileViewController/UIViewController/ProfileViewController.swift
+++ MyPets/MyPets/ProfileViewController/ProfileViewController/UIViewController/ProfileViewController.swift
@@ -15,7 +15,7 @@ class ProfileViewController: UIViewController, GeneralSetupProtocol {
         ["Питомцы", "Архив"],
         ["Подсказки и советы", "Напоминания"],
         ["MyPets Premium"],
-        ["Поддержка", "О приложении", "Skip_launch_status", "Skip_paid_status"]
+        ["Поддержка", "О приложении", "Skip_launch_status", "Skip_paid_status", "Remove_first_entity"]
     ]
     let tipsSwitch = UISwitch()
     let reminderSwitch = UISwitch()
