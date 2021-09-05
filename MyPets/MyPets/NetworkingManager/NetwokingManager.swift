0cd053e20b7b8ec09cf10ae701effa81bdc742d3
AhmerovDmitry <ahmerov.dmitry@gmail.com>
2021-09-05 13:18:26 +0300

    Добавил нетворк менеджер
    Менеджер представляет из себя синглтон для запросов в сеть

--- MyPets/MyPets/NetworkingManager/NetwokingManager.swift
+++ MyPets/MyPets/NetworkingManager/NetwokingManager.swift
@@ -19,28 +19,10 @@ final class NetworkingManager {
                 completion(.failure(error))
             }
             if let data = data {
-                completion(.success(data))
+                DispatchQueue.main.async {
+                    completion(.success(data))
+                }
             }
         }).resume()
     }
 }
-
-/*
-let config = URLSessionConfiguration.default
-config.httpAdditionalHeaders = [
-    "secret-key": "$2b$10$Fk4etsK4fRpWVHR/RDgUOurwV7bW10aCC2rT4M13xO6CgnY4Bphbi"
-]
-let session = URLSession(configuration: config)
-let stringUrl = URL(string: "https://api.jsonbin.io/b/6127b582076a223676b18d03/1")
-session.dataTask(with: stringUrl!, completionHandler: { data, _, error in
-    if let error = error {
-        let alert = UIAlertController(title: "Упс, что-то пошло не по плану!",
-                                      message: "Проверьте интернет соединение на своём устройстве, информация попробует обновиться сама через некоторое время",
-                                      preferredStyle: .alert)
-
-    }
-    if let data = data {
-        print(String(data: data, encoding: .utf8))
-    }
-}).resume()
-*/
