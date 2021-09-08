7654f3dd93f7145d8ee890a24443d082516dc595
AhmerovDmitry <ahmerov.dmitry@gmail.com>
2021-09-08 21:49:51 +0300

    Изменил сервис для новой конфигурации

--- MyPets/MyPets/Services/NetworkService.swift
+++ MyPets/MyPets/Services/NetworkService.swift
@@ -14,14 +14,18 @@ protocol NetworkServiceProtocol {
 
 final class NetworkService {
     private let decoder = JSONDecoder()
-    private let session = URLSession.shared
+    private var session = URLSession.shared
 }
 
 extension NetworkService: NetworkServiceProtocol {
     func loadJSONData<T: Codable>(from url: URL, httpAdditionalHeaders: [AnyHashable: Any]?, decodeModel: T.Type,
                          completion: @escaping (Result<T, Error>) -> Void) {
 
-        session.configuration.httpAdditionalHeaders = httpAdditionalHeaders
+        if let header = httpAdditionalHeaders {
+            let config = URLSessionConfiguration.default
+            config.httpAdditionalHeaders = header
+            session = URLSession(configuration: config)
+        }
 
         session.dataTask(with: url, completionHandler: { [weak self] data, _, error in
             do {
@@ -30,9 +34,8 @@ extension NetworkService: NetworkServiceProtocol {
                         completion(.success(data))
                     }
                 }
-            } catch let error as NSError {
+            } catch {
                 completion(.failure(error))
-                print(error.localizedDescription)
             }
         }).resume()
     }
