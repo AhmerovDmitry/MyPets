8935f5ca85c93fc38c6202ae28099b39220d4114
AhmerovDmitry <ahmerov.dmitry@gmail.com>
2021-09-10 13:16:54 +0300

    Добавил функцию получения координат
    Добавил функцию которая изменяет путь запроса в связи с координатами пользователя

--- MyPets/MyPets/MainMenuModule/Model/MainWeatherModel.swift
+++ MyPets/MyPets/MainMenuModule/Model/MainWeatherModel.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 struct MainWeatherModel {
+
     /// Поля для погодного API
     private let urlSite = "https://api.openweathermap.org/data/2.5/weather?"
-    private let userLat = "lat=58.025995"
-    private let userLon = "lon=38.928622"
+    private var userLat = ""
+    private var userLon = ""
     private let userConfig = "units=metric"
     private let urlKey = "appid=10dff189d22023f2127fd521780e2b2d"
     var weatherURL: URL? {
@@ -22,4 +23,9 @@ struct MainWeatherModel {
     /// Поля для собственного API
     let httpAdditionalHeaders = ["secret-key": "$2b$10$Fk4etsK4fRpWVHR/RDgUOurwV7bW10aCC2rT4M13xO6CgnY4Bphbi"]
     let imagesURL = URL(string: "https://api.jsonbin.io/b/6127b582076a223676b18d03/6")
+
+    mutating func setUserCoordinate(lat: String, lon: String) {
+        userLat = "lat=" + lat
+        userLon = "lon=" + lon
+    }
 }
