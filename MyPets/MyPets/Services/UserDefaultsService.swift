6ff83f51dcd4a4fd94625a85336872cfb6c19b2e
AhmerovDmitry <ahmerov.dmitry@gmail.com>
2021-09-12 02:41:40 +0300

    Добавил UserDefaultsService

--- MyPets/MyPets/Services/UserDefaultsService.swift
+++ MyPets/MyPets/Services/UserDefaultsService.swift
@@ -7,23 +7,59 @@
 
 import UIKit
 
-extension UserDefaults {
+/*
+protocol UserDefaultsServiceProtocol {
+    func isFirstLaunch() -> Bool
+    func isAppNotPurchased() -> Bool
 
-    static func isFirstLaunch() -> Bool {
-        let isFirstLaunch = UserDefaults.standard.bool(forKey: "isFirstLaunch")
+    func setStandardKey(_ key: String)
+}
+
+final class UserDefaultsService: UserDefaultsServiceProtocol {
+
+    /// Метод проверяющий первый запуск приложения
+    /// - Returns: Возвращаемое булевое значение
+    func isFirstLaunch() -> Bool {
+        let isFirstLaunch = !UserDefaults.standard.bool(forKey: "isFirstLaunch")
         if isFirstLaunch {
-            UserDefaults.standard.set(true, forKey: "isFirstLaunch")
-            UserDefaults.standard.synchronize()
+            setStandardKey("isFirstLaunch")
+//            UserDefaults.standard.synchronize()
         }
         return isFirstLaunch
     }
 
-    static func appPaidStatus() -> Bool {
-        let paidStatus = UserDefaults.standard.bool(forKey: "paidStatus")
+    func setStandardKey(_ key: String) {
+        UserDefaults.standard.set(true, forKey: key)
+    }
+
+    /// Метод проверяющий статус покупки приложения
+    /// - Returns: Возвращаемое булевое значение
+    func isAppNotPurchased() -> Bool {
+        let paidStatus = !UserDefaults.standard.bool(forKey: "paidStatus")
         if paidStatus {
-            UserDefaults.standard.set(true, forKey: "paidStatus")
-            UserDefaults.standard.synchronize()
+            setStandardKey("paidStatus")
+//            UserDefaults.standard.synchronize()
         }
         return paidStatus
+    }
+}
+*/
+
+enum RawValueType: String {
+    case isFirstLaunch = "isNotFirstLaunch"
+    case isAppPurchased = "isAppPurchased"
+}
+
+protocol UserDefaultsServiceProtocol {
+    func value(forKey key: String) -> Bool
+    func setValue(_ value: Bool, forKey key: String)
+}
+
+final class UserDefaultsService: UserDefaultsServiceProtocol {
+    func value(forKey key: String) -> Bool {
+        return UserDefaults.standard.bool(forKey: key)
+    }
+    func setValue(_ value: Bool, forKey key: String) {
+        UserDefaults.standard.setValue(value, forKey: key)
     }
 }
