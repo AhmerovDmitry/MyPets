a0d3d23c18e2db09a1dd40791604aba8820aa4a6
AhmerovDmitry <ahmerov.dmitry@gmail.com>
2021-09-11 17:47:42 +0300

    Удалил пустую папку тестов

--- MyPets/MyPetsUITests/MyPetsUITests.swift
+++ MyPets/MyPetsUITests/MyPetsUITests.swift
@@ -2,24 +2,41 @@
 //  MyPetsUITests.swift
 //  MyPetsUITests
 //
-//  Created by Дмитрий Ахмеров on 27.08.2021.
+//  Created by Дмитрий Ахмеров on 11.09.2021.
 //
 
 import XCTest
-@testable import MyPets
 
 class MyPetsUITests: XCTestCase {
-    var app: XCUIApplication!
 
     override func setUpWithError() throws {
-        try super.setUpWithError()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        app = XCUIApplication()
-        app.launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    func testCloseButtonWorkAndClosedScreen() {
-//        button.accessibilityIdentifier = "skipButton" задается в настройках создания объекта
-//        app.buttons["skipButton"].tap()
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
     }
 }
