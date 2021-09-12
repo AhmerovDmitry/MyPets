8641e717b2c68493cb027c74863b5cfddab10e98
AhmerovDmitry <ahmerov.dmitry@gmail.com>
2021-09-12 23:22:52 +0300

    Убрал лишний код, изменил функции
    Теперь не требуется вводить значения самостоятельно, они выбираются с помощью enum

--- MyPets/MyPets/Services/UserDefaultsService.swift
+++ MyPets/MyPets/Services/UserDefaultsService.swift
@@ -7,59 +7,34 @@
 
 import UIKit
 
-/*
-protocol UserDefaultsServiceProtocol {
-    func isFirstLaunch() -> Bool
-    func isAppNotPurchased() -> Bool
+protocol KeyDescriptionProtocol {
+    var description: String { get }
+}
 
-    func setStandardKey(_ key: String)
+enum KeyRawValue: KeyDescriptionProtocol {
+    case isNotFirstLaunch, isAppPurchased
+
+    var description: String {
+        switch self {
+        case .isNotFirstLaunch:
+            return "isNotFirstLaunch"
+        case .isAppPurchased:
+            return "isAppPurchased"
+        }
+    }
+}
+
+protocol UserDefaultsServiceProtocol {
+    func value(forKey key: KeyRawValue) -> Bool
+    func setValue(_ value: Bool, forKey key: KeyRawValue)
 }
 
 final class UserDefaultsService: UserDefaultsServiceProtocol {
 
-    /// Метод проверяющий первый запуск приложения
-    /// - Returns: Возвращаемое булевое значение
-    func isFirstLaunch() -> Bool {
-        let isFirstLaunch = !UserDefaults.standard.bool(forKey: "isFirstLaunch")
-        if isFirstLaunch {
-            setStandardKey("isFirstLaunch")
-//            UserDefaults.standard.synchronize()
-        }
-        return isFirstLaunch
+    func value(forKey key: KeyRawValue) -> Bool {
+        return UserDefaults.standard.bool(forKey: key.description)
     }
-
-    func setStandardKey(_ key: String) {
-        UserDefaults.standard.set(true, forKey: key)
-    }
-
-    /// Метод проверяющий статус покупки приложения
-    /// - Returns: Возвращаемое булевое значение
-    func isAppNotPurchased() -> Bool {
-        let paidStatus = !UserDefaults.standard.bool(forKey: "paidStatus")
-        if paidStatus {
-            setStandardKey("paidStatus")
-//            UserDefaults.standard.synchronize()
-        }
-        return paidStatus
-    }
-}
-*/
-
-enum RawValueType: String {
-    case isFirstLaunch = "isNotFirstLaunch"
-    case isAppPurchased = "isAppPurchased"
-}
-
-protocol UserDefaultsServiceProtocol {
-    func value(forKey key: String) -> Bool
-    func setValue(_ value: Bool, forKey key: String)
-}
-
-final class UserDefaultsService: UserDefaultsServiceProtocol {
-    func value(forKey key: String) -> Bool {
-        return UserDefaults.standard.bool(forKey: key)
-    }
-    func setValue(_ value: Bool, forKey key: String) {
-        UserDefaults.standard.setValue(value, forKey: key)
+    func setValue(_ value: Bool, forKey key: KeyRawValue) {
+        UserDefaults.standard.setValue(value, forKey: key.description)
     }
 }
