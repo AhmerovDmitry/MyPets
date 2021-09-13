0d148e9f4de3a2931bf1a79d234ab22b038b1d6d
AhmerovDmitry <ahmerov.dmitry@gmail.com>
2021-09-14 01:45:26 +0300

    Добавил статическую константу для высоты ячейки

---
+++ MyPets/MyPets/Extension/UITableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  UITableViewCell.swift
+//  MyPets
+//
+//  Created by Дмитрий Ахмеров on 13.09.2021.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    static let heightSubRow: CGFloat = 40
+}
