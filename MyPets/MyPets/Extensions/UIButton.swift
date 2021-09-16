31eb0e316fe880ee703da45bc94952d1da5f1dd8
AhmerovDmitry <ahmerov.dmitry@gmail.com>
2021-09-16 20:07:33 +0300

    Добавил билдер для стандартной кнопки

--- MyPets/MyPets/Extensions/UIButton.swift
+++ MyPets/MyPets/Extensions/UIButton.swift
@@ -7,26 +7,63 @@
 
 import UIKit
 
-extension UIButton {
-    static func createStandartButton(title: String,
-                                     backgroundColor: UIColor,
-                                     action: Selector,
-                                     target: Any?) -> UIButton {
+final class TypicalProjectButtonBuilder {
+    private var title: String?
+    private var titleColor: UIColor?
+    private var backgroundColor: UIColor?
+    private var font: UIFont?
+    private var adjustsFontSizeToFitWidth: Bool?
+    private var borderWidth: CGFloat?
+    private var borderColor: CGColor?
+    private var action: Selector?
+    private var target: Any?
+
+    func with(title: String) -> TypicalProjectButtonBuilder {
+        self.title = title
+        return self
+    }
+    func with(titleColor: UIColor) -> TypicalProjectButtonBuilder {
+        self.titleColor = titleColor
+        return self
+    }
+    func with(backgroundColor: UIColor) -> TypicalProjectButtonBuilder {
+        self.backgroundColor = backgroundColor
+        return self
+    }
+    func with(font: UIFont) -> TypicalProjectButtonBuilder {
+        self.font = font
+        return self
+    }
+    func with(adjustsFontSizeToFitWidth: Bool) -> TypicalProjectButtonBuilder {
+        self.adjustsFontSizeToFitWidth = adjustsFontSizeToFitWidth
+        return self
+    }
+    func with(borderWidth: CGFloat) -> TypicalProjectButtonBuilder {
+        self.borderWidth = borderWidth
+        return self
+    }
+    func with(borderColor: CGColor) -> TypicalProjectButtonBuilder {
+        self.borderColor = borderColor
+        return self
+    }
+    func with(_ target: Any, action: Selector) -> TypicalProjectButtonBuilder {
+        self.target = target
+        self.action = action
+        return self
+    }
+
+    func build() -> UIButton {
         let button = UIButton(type: .system)
-        button.layer.cornerRadius = button.bounds.height / 2
         button.setTitle(title, for: .normal)
+        button.setTitleColor(titleColor, for: .normal)
         button.backgroundColor = backgroundColor
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .regular)
-        button.titleLabel?.adjustsFontSizeToFitWidth = true
-        button.layer.borderWidth = 1
-        if button.backgroundColor == .white {
-            button.setTitleColor(UIColor.CustomColor.purple, for: .normal)
-            button.layer.borderColor = UIColor.CustomColor.purple.cgColor
-        } else {
-            button.setTitleColor(.white, for: .normal)
-            button.layer.borderColor = UIColor.white.cgColor
+        button.titleLabel?.font = font
+        button.titleLabel?.adjustsFontSizeToFitWidth = adjustsFontSizeToFitWidth ?? false
+        button.layer.borderWidth = borderWidth ?? 0
+        button.layer.borderColor = borderColor ?? UIColor.clear.cgColor
+        if let action = action {
+            button.addTarget(target, action: action, for: .touchUpInside)
         }
-        button.addTarget(target, action: action, for: .touchUpInside)
         return button
     }
 }
