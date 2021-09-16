f978c0b848ce2f7bc2484626f327bc21ffc1c6b4
AhmerovDmitry <ahmerov.dmitry@gmail.com>
2021-09-16 20:07:15 +0300

    Добавил таргет с Unit тестами

---
+++ MyPets/MyPetsUnitTests/MyPetsUnitTests.swift
@@ -0,0 +1,15 @@
+//
+//  MyPetsUnitTests.swift
+//  MyPetsUnitTests
+//
+//  Created by Дмитрий Ахмеров on 16.09.2021.
+//
+
+import XCTest
+@testable import MyPets
+
+class MyPetsUnitTests: XCTestCase {
+
+    
+
+}
